792187f408f936eb2c8fb789bd059fdf67b7326b
Vladislav Lykhoiedenko <v.lykhoiedenko@gmail.com>
2025-06-24 13:55:20 +0300

    Solutio

--- app/main.py
+++ app/main.py
@@ -2,87 +2,68 @@ import math
 
 
 class Vector:
-    def __init__(self, x_coordinate: float, y_coordinate: float) -> None:
-        self.x_coordinate = round(x_coordinate, 2)
-        self.y_coordinate = round(y_coordinate, 2)
+    def __init__(self, x: float, y: float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
 
-    def __add__(self, other: "Vector") -> "Vector":
+    def __add__(self, other) -> "Vector":
         if isinstance(other, Vector):
-            new_x = self.x_coordinate + other.x_coordinate
-            new_y = self.y_coordinate + other.y_coordinate
-            return Vector(new_x, new_y)
-        return NotImplemented
+            x = self.x + other.x
+            y = self.y + other.y
+        else:
+            return NotImplemented
+        return Vector(x, y)
 
-    def __sub__(self, other: "Vector") -> "Vector":
+    def __sub__(self, other):
         if isinstance(other, Vector):
-            new_x = self.x_coordinate - other.x_coordinate
-            new_y = self.y_coordinate - other.y_coordinate
-            return Vector(new_x, new_y)
-        return NotImplemented
+            x = self.x - other.x
+            y = self.y - other.y
+        else:
+            return NotImplemented
+        return Vector(x, y)
 
-    def __mul__(self, other: int | float | "Vector") -> float | "Vector":
+    def __mul__(self, other):
         if isinstance(other, Vector):
-            dot_product = (
-                self.x_coordinate * other.x_coordinate
-                + self.y_coordinate * other.y_coordinate
-            )
+            dot_product = self.x * other.x + self.y * other.y
             return dot_product
         elif isinstance(other, (int, float)):
-            return Vector(
-                self.x_coordinate * other, self.y_coordinate * other
-            )
+            return Vector(self.x * other, self.y * other)
         return NotImplemented
 
     @classmethod
-    def create_vector_by_two_points(
-        cls,
-        start_point: tuple[float, float],
-        end_point: tuple[float, float],
-    ) -> "Vector":
+    def create_vector_by_two_points(cls, start_point, end_point):
         x1, y1 = start_point
         x2, y2 = end_point
-        new_x = x2 - x1
-        new_y = y2 - y1
-        return cls(new_x, new_y)
+        x = x2 - x1
+        y = y2 - y1
+        return cls(x, y)
 
-    def get_length(self) -> float:
-        return math.sqrt(
-            self.x_coordinate ** 2 + self.y_coordinate ** 2
-        )
+    def get_length(self):
+        length = math.sqrt(self.x ** 2 + self.y ** 2)
+        return length
 
-    def get_normalized(self) -> "Vector":
+    def get_normalized(self):
         length = self.get_length()
-        return Vector(
-            self.x_coordinate / length,
-            self.y_coordinate / length,
-        )
+        return Vector(self.x / length, self.y / length)
 
-    def angle_between(self, other: "Vector") -> int:
-        dot = (
-            self.x_coordinate * other.x_coordinate
-            + self.y_coordinate * other.y_coordinate
-        )
-        length1 = self.get_length()
-        length2 = other.get_length()
+    def angle_between(self, other):
+        dot = (self.x * other.x) + (self.y * other.y)
+        length1 = math.sqrt(self.x ** 2 + self.y ** 2)
+        length2 = math.sqrt(other.x ** 2 + other.y ** 2)
         cos_alpha = dot / (length1 * length2)
         angle_in_degrees = math.degrees(math.acos(cos_alpha))
         return round(angle_in_degrees)
 
-    def get_angle(self) -> int:
-        dot = self.y_coordinate
-        length1 = self.get_length()
-        cos_alpha = dot / length1
+    def get_angle(self):
+        dot = (self.x * 0) + (self.y * 1)
+        length1 = math.sqrt(self.x ** 2 + self.y ** 2)
+        length2 = math.sqrt(0 ** 2 + 1 ** 2)
+        cos_alpha = dot / (length1 * length2)
         angle = math.degrees(math.acos(cos_alpha))
         return round(angle)
 
-    def rotate(self, degrees: int | float) -> "Vector":
+    def rotate(self, degrees: int | float):
         radians = math.radians(degrees)
-        new_x = (
-            self.x_coordinate * math.cos(radians)
-            - self.y_coordinate * math.sin(radians)
-        )
-        new_y = (
-            self.x_coordinate * math.sin(radians)
-            + self.y_coordinate * math.cos(radians)
-        )
-        return Vector(new_x, new_y)
+        x = self.x * math.cos(radians) - self.y * math.sin(radians)
+        y = self.x * math.sin(radians) + self.y * math.cos(radians)
+        return Vector(x, y)
